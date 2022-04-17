5ab03670efd9f389907fd7eeb29bfd1cef4624f9
Juliano Ramos <juliano_nj@hotmail.com>
2022-04-17 17:49:01 +0100

    commit inicial

--- devflix/settings.py
+++ devflix/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,7 +87,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
