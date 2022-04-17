468467de4a4809571f80b574265c19a9a334c7d8
Juliano Ramos <juliano_nj@hotmail.com>
2022-04-17 17:57:05 +0100

    ajuste staticfiles

--- devflix/settings.py
+++ devflix/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
